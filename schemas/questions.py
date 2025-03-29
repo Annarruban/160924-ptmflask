c44e0b817622414963ad737e40429d84458d9073
annaruban <anna14196@ukr.net>
2025-03-29 18:38:14 +0100

    dev: add category to pydantic

--- schemas/questions.py
+++ schemas/questions.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel, Field, ConfigDict
 
 
+class CategoryBase(BaseModel):
+    id: int
+    name: str
+
+
 class QuestionCreate(BaseModel):
     text: str = Field(..., min_length=10) # ... = required
     category_id: int = Field(gt=0)
@@ -9,6 +14,7 @@ class QuestionCreate(BaseModel):
 class QuestionResponse(BaseModel):
     id: int
     text: str
+    category: CategoryBase
     category_id: int | None = None
 
     model_config = ConfigDict(
